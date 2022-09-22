a8c34da569431b413aabf3a949e27afd6ef0cbbe
Abby <abbychoi97@gmail.com>
2022-09-22 14:01:38 -0700

    refactor merge_lists functions

--- function_c.py
+++ function_c.py
@@ -3,7 +3,7 @@ def merge_lists(list_a, list_b):
     a combination of list_a and list_b
     without any duplicate elements.
     """
-    return list(set(list_a).union(set(list_b)))
+    return list(set(list_a) | set(list_b))
 
 if __name__ == "__main__":
     print(merge_lists([1, 1, 2, 3], [3, 4, 5]))
